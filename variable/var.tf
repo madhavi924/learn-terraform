d5ba52acd6c5873e753d622b27b5e0c033b73f73
madhavi <madhavilatha.008@gmail.com>
2023-04-25 15:55:46 -0500

    terraform

--- variable/var.tf
+++ variable/var.tf
@@ -13,6 +13,6 @@ output "sample_string" {
 
 ## in the above we are printing just variable, if variable is a combination of some strings then following is the syntax. ${} is mandatory if we include the variable inside a string
 
-output "sample string1" {
+output "sample_string1" {
   value = "value of sample_string = ${var.sample_string}"
 }
