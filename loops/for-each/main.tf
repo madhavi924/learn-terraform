9d2bdae170cae4625072826ba6b36e3d4af063f3
madhavi <madhavilatha.008@gmail.com>
2023-05-02 11:33:22 -0500

    loops

--- loops/for-each/main.tf
+++ loops/for-each/main.tf
@@ -1,10 +1,12 @@
 resource "null_resource" "fruits" {
   provisioner "local-exec" {
     command = " echo ${ length(var.fruits)}"
-  }}
-variable "fruits"{
-  default={
-    apple = 10
+  }
+}
+variable "fruits" {
+  default = {
+    apple  = 10
     orange = 200
     banana = 100
   }
+}
