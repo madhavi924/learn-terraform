9d111a565fc6f835c859bce3089c891b1bf96748
madhavi <madhavilatha.008@gmail.com>
2023-05-04 15:33:50 -0500

    terraform

--- functions/main.tf
+++ functions/main.tf
@@ -6,3 +6,11 @@ variable "sample" {
 output "sample" {
   value = upper(var.sample)
 }
+
+variable "sample1" {
+  default = ["abc" "xyz"]
+}
+
+output "sample" {
+  value = element(var.sample1,0)
+}
