21956e19e62f3cef083212729f2b3f3ae66f09b8
madhavi <madhavilatha.008@gmail.com>
2023-05-05 15:51:27 -0500

    terraform

--- MODULE/module/main.tf
+++ MODULE/module/main.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "nothing" {
   provisioner "local-exec" {
-    command = "echo Hello from module"
+    command = "echo Hello from module - ${var.input}"
   }
 }
 variable "input"{}
