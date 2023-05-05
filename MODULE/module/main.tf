82f32f136bdfe0e476a80652404f04e6664bb658
madhavi <madhavilatha.008@gmail.com>
2023-05-05 15:49:16 -0500

    terraform

--- MODULE/module/main.tf
+++ MODULE/module/main.tf
@@ -4,15 +4,3 @@ resource "null_resource" "nothing" {
   }
 }
 
-resource "null_resource" "nothing1" {
-  provisioner "local-exec" {
-    command = "echo Hello from module"
-  }
-}
-
-
-resource "null_resource" "nothing2" {
-  provisioner "local-exec" {
-    command = "echo Hello from module"
-  }
-}
