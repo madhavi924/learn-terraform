58df586b0f1fc5256ccc3d4d2716f0b839d176b2
madhavi <madhavilatha.008@gmail.com>
2023-08-15 15:23:50 -0500

    terraform

--- example/sample.tf
+++ example/sample.tf
@@ -1,8 +1,12 @@
-resource "aws_instance" "web" {
-  ami           = "ami-03265a0778a880afb"
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "HelloWorld"
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.12.0"
+    }
   }
+}
+
+provider "aws" {
+  # Configuration options
 }
