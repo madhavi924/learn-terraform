a89a70b2dde6f8e2d76de858885010199e07df6d
madhavi <madhavilatha.008@gmail.com>
2023-08-15 14:52:22 -0500

    terraform

--- example/sample.tf
+++ example/sample.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "web" {
   ami           = "ami-03265a0778a880afb"
-  instance_type = "t3.small"
+  instance_type = "t3.micro"
+
   tags = {
-    Name = "Helloworld"
+    Name = "HelloWorld"
   }
 }
