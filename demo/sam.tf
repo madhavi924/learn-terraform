72afe865140e2cd39dc0a2557dbb09bde21d3d26
madhavi <madhavilatha.008@gmail.com>
2023-08-16 15:45:00 -0500

    terraform

--- demo/sam.tf
+++ demo/sam.tf
@@ -11,3 +11,12 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_instance" "web" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
