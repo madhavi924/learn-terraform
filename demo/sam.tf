52dae1b499dbd75e08007e48e2576497c6f3e038
madhavi <madhavilatha.008@gmail.com>
2023-08-16 15:31:52 -0500

    terraform

--- demo/sam.tf
+++ demo/sam.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "web" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
-  vpc_security_group_ids = [var.sg_id]
 
   tags = {
     Name = "HelloWorld"
