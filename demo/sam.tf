0c24a14487c48801bb011361316b5e9f363de66e
madhavi <madhavilatha.008@gmail.com>
2023-08-16 15:58:46 -0500

    terraform

--- demo/sam.tf
+++ demo/sam.tf
@@ -6,13 +6,5 @@ resource "aws_instance" "test" {
     Name = "HelloWorld"
   }
 }
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
 
 
