cc5f43e1d81d290a209f614c7a5391f8e38ec925
madhavi <madhavilatha.008@gmail.com>
2023-08-16 15:40:37 -0500

    terraform

--- demo/sam.tf
+++ demo/sam.tf
@@ -1,15 +1,13 @@
-resource "aws_instance" "web" {
-  ami           = "ami-03265a0778a880afb"
-  instance_type = "t3.micro"
-  vpc_security_group_ids = [var.sg_id]
-
-
-  tags = {
-    Name = "HelloWorld"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
 }
+
+# Configure the AWS Provider
 provider "aws" {
-  version = "~> 5.0"
-  region  = "us-east-1"
+  region = "us-east-1"
 }
-variable "sg_id" {}
