a876a9cdb560380c7f5ca349e7ccede9b9e8f4c8
madhavi <madhavilatha.008@gmail.com>
2023-08-16 15:59:45 -0500

    terraform

--- demo/sam.tf
+++ demo/sam.tf
@@ -1,10 +1,6 @@
-resource "aws_instance" "test" {
-  ami           = "ami-03265a0778a880afb"
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "HelloWorld"
-  }
+data "aws_ec2_instance_type" "example" {
+  instance_type = "t2.micro"
 }
+
 
 
