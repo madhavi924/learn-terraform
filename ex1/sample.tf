1ce6b00a42396676fbf5b3c179639e6f7488652c
madhavi <madhavilatha.008@gmail.com>
2023-08-17 16:10:44 -0500

    terraform

--- ex1/sample.tf
+++ ex1/sample.tf
@@ -15,6 +15,14 @@ output "ami" {
   }
 }
 
+ resource "aws_route53_record" "frontend" {
+   zone_id = "Z07268141ZWT4Z47HHYHW"
+   name    = "frontend-dev.madhavi91.online"
+   type    = "A"
+   ttl     = 30
+   records = [aws_instance.frontend.private_ip]
+ }
+
 resource "aws_instance" "mongodb" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
@@ -23,7 +31,16 @@ resource "aws_instance" "mongodb" {
   }
 }
 
-resource "aws_instance" "catalogue" {
+ resource "aws_route53_record" "mongodb" {
+   zone_id = "Z07268141ZWT4Z47HHYHW"
+   name    = "mongodb-dev.madhavi91.online"
+   type    = "A"
+   ttl     = 30
+   records = [aws_instance.mongodb.private_ip]
+ }
+
+
+ resource "aws_instance" "catalogue" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
   tags = {
@@ -31,7 +48,16 @@ resource "aws_instance" "catalogue" {
   }
 }
 
-resource "aws_instance" "redis" {
+ resource "aws_route53_record" "catalogue" {
+   zone_id = "Z07268141ZWT4Z47HHYHW"
+   name    = "catalogue-dev.madhavi91.online"
+   type    = "A"
+   ttl     = 30
+   records = [aws_instance.catalogue.private_ip]
+ }
+
+
+ resource "aws_instance" "redis" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
   tags = {
@@ -39,7 +65,16 @@ resource "aws_instance" "redis" {
   }
 }
 
-resource "aws_instance" "user" {
+
+ resource "aws_route53_record" "radis" {
+   zone_id = "Z07268141ZWT4Z47HHYHW"
+   name    = "radis-dev.madhavi91.online"
+   type    = "A"
+   ttl     = 30
+   records = [aws_instance.redis.private_ip]
+ }
+
+ resource "aws_instance" "user" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
   tags = {
@@ -47,7 +82,16 @@ resource "aws_instance" "user" {
   }
 }
 
-resource "aws_instance" "cart" {
+ resource "aws_route53_record" "user" {
+   zone_id = "Z07268141ZWT4Z47HHYHW"
+   name    = "user-dev.madhavi91.online"
+   type    = "A"
+   ttl     = 30
+   records = [aws_instance.user.private_ip]
+ }
+
+
+ resource "aws_instance" "cart" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
   tags = {
@@ -55,7 +99,16 @@ resource "aws_instance" "cart" {
   }
 }
 
-resource "aws_instance" "mysql" {
+ resource "aws_route53_record" "cart" {
+   zone_id = "Z07268141ZWT4Z47HHYHW"
+   name    = "cart-dev.madhavi91.online"
+   type    = "A"
+   ttl     = 30
+   records = [aws_instance.cart.private_ip]
+ }
+
+
+ resource "aws_instance" "mysql" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
   tags = {
@@ -63,7 +116,16 @@ resource "aws_instance" "mysql" {
   }
 }
 
-resource "aws_instance" "shipping" {
+ resource "aws_route53_record" "mysql" {
+   zone_id = "Z07268141ZWT4Z47HHYHW"
+   name    = "mysql-dev.madhavi91.online"
+   type    = "A"
+   ttl     = 30
+   records = [aws_instance.mysql.private_ip]
+ }
+
+
+ resource "aws_instance" "shipping" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
   tags = {
@@ -71,7 +133,16 @@ resource "aws_instance" "shipping" {
   }
 }
 
-resource "aws_instance" "RabbitMQ" {
+
+ resource "aws_route53_record" "shipping" {
+   zone_id = "Z07268141ZWT4Z47HHYHW"
+   name    = "shipping-dev.madhavi91.online"
+   type    = "A"
+   ttl     = 30
+   records = [aws_instance.shipping.private_ip]
+ }
+
+ resource "aws_instance" "RabbitMQ" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
   tags = {
@@ -79,10 +150,25 @@ resource "aws_instance" "RabbitMQ" {
   }
 }
 
-resource "aws_instance" "payment" {
+ resource "aws_route53_record" "RabbitMQ" {
+   zone_id = "Z07268141ZWT4Z47HHYHW"
+   name    = "RabbitMQ-dev.madhavi91.online"
+   type    = "A"
+   ttl     = 30
+   records = [aws_instance.RabbitMQ.private_ip]
+ }
+
+ resource "aws_instance" "payment" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
   tags = {
     Name = "payment"
   }
-}
+}resource "aws_route53_record" "payment" {
+   zone_id = "Z07268141ZWT4Z47HHYHW"
+   name    = "payment-dev.madhavi91.online"
+   type    = "A"
+   ttl     = 30
+   records = [aws_instance.payment.private_ip]
+ }
+
